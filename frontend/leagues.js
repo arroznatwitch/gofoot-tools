1e7da3e7f489f965732492f0e956e630a23c8c8c
Cardoso <ruee.contato@gmail.com>
2026-07-18 20:41:34 +0100

    atualizado nome das ligas

--- frontend/leagues.js
+++ frontend/leagues.js
@@ -1,18 +1,18 @@
 window.LEAGUES_DATA = {
     "Brasil": {
-        "Liga Brasileira 26/27": [
+        "Série A - 26/27": [
             "São Paulo", "Botafogo", "Vitória", "Corinthians", "Santos",
             "Palmeiras", "Remo", "Bahia", "Coritiba", "RB Bragantino", "Atlético Paranaense",
             "Mirassol", "Chapecoense", "Flamengo", "Cruzeiro", "Atlético Mineiro",
             "Grêmio", "Internacional", "Fluminense", "Vasco", 
         ],
-        "Segunda Liga Brasileira 26/27": [
+        "Série B - 26/27": [
             "América Mineiro", "Athletic Club", "Atlético Goianiense", "Avaí", 
             "Botafogo-SP", "Ceará", "CRB", "Criciúma", "Cuiabá", "Fortaleza",
             "Goiás", "Juventude", "Londrina", "Naútico", "Novorizontino", 
             "Operário-PR", "Ponte Preta", "São Bernardo", "Sport Recife", "Vila Nova",
         ],
-        "Terceira Liga Brasileira 26/27": [
+        "Série C - 26/27": [
         "Amazonas", "Anápolis", "Barra", "Botafogo-PB", "Brusque",
         "Caxias", "Confiança", "Ferroviária", "Figueirense", "Floresta",
         "Guarani", "Inter De Limeira", "Itabaiana", "Ituano", "Maranhão",
@@ -20,7 +20,7 @@ window.LEAGUES_DATA = {
     ]
     },
     "Portugal": {
-        "Liga Portuguesa 26/27": [
+        "Liga Portuguesa Betclic- 26/27": [
             "FC Porto", "Benfica", "Sporting CP", "SC Braga", "Vitória SC", "Santa Clara",
             "Gil Vicente", "Famalicão", "Marítimo", "Estoril Praia", "Rio Ave",
             "Alverca SAD", "Casa Pia", "Arouca", "Moreirense", "Nacional da Madeira",
@@ -28,7 +28,7 @@ window.LEAGUES_DATA = {
         ]
     },
     "Jamaica": {
-        "Liga da Jamaica 26/27": [
+        "Jamaica Premier League - 26/27": [
             "Harbour View", "Chapelton Maroons", "Arnett Gardens", "Portmore Utd",
             "Cavalier", "Dunbeholden", "Mount Pleasant", "Tivoli Gardens",
             "Racing Utd (JAM)", "Montego Bay Utd", "Treasure Beach", "Waterhouse",
